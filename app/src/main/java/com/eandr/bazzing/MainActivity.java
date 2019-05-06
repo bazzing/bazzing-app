4e3210050c9fd7851a034e8889f47e9c9e076692
Eyal Iaroslavich <iaroslavitz.eyal@gmail.com>
2019-05-06 14:30:16 +0300

    add input phone number activity

--- app/src/main/java/com/eandr/bazzing/MainActivity.java
+++ app/src/main/java/com/eandr/bazzing/MainActivity.java
@@ -1,7 +1,13 @@
 package com.eandr.bazzing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.eandr.bazzing.verification.PhoneNumberInputActivity;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        handleUserRegistration();
+    }
+
+    private void handleUserRegistration() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser == null) {
+            startActivity(new Intent(MainActivity.this, PhoneNumberInputActivity.class));
+            finish();
+        }
     }
 }
---
+++ app/src/main/java/com/eandr/bazzing/verification/PhoneNumberInputActivity.java
@@ -0,0 +1,15 @@
+package com.eandr.bazzing.verification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eandr.bazzing.R;
+
+public class PhoneNumberInputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_number_input);
+    }
+}
